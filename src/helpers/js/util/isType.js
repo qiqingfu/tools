5eba90fb70dd6ad8ce0decd447fb5b1458c84cdb
qiqf <723469354@qq.com>
2019-12-03 20:57:40 +0800

    feat: isType

---
+++ src/helpers/js/util/isType.js
@@ -0,0 +1,23 @@
+const Type = (() => {
+  const typeList = [
+    "String",
+    "Number",
+    "Boolean",
+    "Function",
+    "Object",
+    "Array",
+    "Null",
+    "Date"
+  ];
+
+  const type = {};
+
+  for (let i = 0; i < typeList.length; i++) {
+    const val = typeList[i];
+    type[`is${val}`] = obj => {
+      return Array.prototype.toString.call(obj) === `[object ${val}]`;
+    };
+  }
+
+  return type;
+})();
--- src/helpers/js/util/throttle.js
+++ src/helpers/js/util/throttle.js
@@ -2,34 +2,33 @@
  * 函数节流
  * 原理: 持续触发事件, 每隔一段事件, 只执行一次事件
  * 通过第三个 options 参数配置首次和结束是否执行
- * 
- * @param {Function} func 
- * @param {Number*} wait 
- * @param {Object*} options 
+ *
+ * @param {Function} func
+ * @param {Number*} wait
+ * @param {Object*} options
  * @param {Boolean*} start
  * @param {Boolean*} end
  */
- const throttle = (func, wait = 300, options = {}) => {
+const throttle = (func, wait = 300, options = {}) => {
   let context, timer, args;
 
   // 记录上一次的时间戳, 初始为0
-  let previous = 0
+  let previous = 0;
 
   // 定时执行的定时器函数
-  const later = function () {
-    previous = options.start === false ? 0 : + new Date()
-    timer = null
-    func.apply(context, args)
-    if (timer) context = args = null
-  }
+  const later = function() {
+    previous = options.start === false ? 0 : +new Date();
+    timer = null;
+    func.apply(context, args);
+    if (timer) context = args = null;
+  };
 
-  return function () {
-
+  return function() {
     // 每次触发事件, 获取一次最新的时间戳
-    let now = + new Date()
+    let now = +new Date();
 
     // 不立即执行的情况下, 将 previous 更新为最新的时间戳
-    if (!previous && options.start === false) previous = now
+    if (!previous && options.start === false) previous = now;
 
     /**
      * 不立即执行的情况下
@@ -37,21 +36,21 @@
      *  2. 再次触发事件时, previous 保持之前的时间戳, now为最新的时间戳
      *  3. diff 的值逐渐变小, 直到 <=0
      */
-    let diff = wait - (now - previous)
-    context = this
-    args = arguments
+    let diff = wait - (now - previous);
+    context = this;
+    args = arguments;
     if (diff <= 0 || diff > wait) {
       if (timer) {
-        clearTimeout(timer)
-        timer = null
+        clearTimeout(timer);
+        timer = null;
       }
 
       // 更新为上一次的时间戳
-      previous = now
-      func.apply(func, args)
-      if (!timer) context = args = null
+      previous = now;
+      func.apply(func, args);
+      if (!timer) context = args = null;
     } else if (!timer && options.end !== false) {
-      timer = setTimeout(later, diff)
+      timer = setTimeout(later, diff);
     }
-  }
- }
+  };
+};
