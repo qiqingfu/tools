3b0e261e80fdfce1a1e6f54b05eb7629f6e1b2ec
qiqf <723469354@qq.com>
2020-02-19 22:31:50 +0800

    update: crypto 重写对称加密函数

--- src/helpers/js/util/crypto.js
+++ src/helpers/js/util/crypto.js
@@ -1,26 +1,45 @@
-const crypto = require('crypto');
-const iv = crypto.randomBytes(128/8);
+/**
+ * @author qiqingfu
+ * @date 2020-02-19 21:48
+ */
 
-module.exports = {
-  // symmetric encrypted data
-  encryption(data) {
-    const key = crypto.randomBytes(192/8);
-    let enc = '';
-    let cipher = crypto.createCipheriv('aes192', key, iv);
-    enc += cipher.update(data, 'utf-8', 'hex');
-    enc += cipher.final('hex');
+/**
+ * 对称加密
+ */
 
-    return {
-      enc,
-      key
-    };
-  },
-  // symmetric decryption data
-  decryption(data, key) {
-    let decipher = crypto.createDecipheriv('aes192', key, iv);
-    let dec = decipher.update(data, 'hex', 'utf-8');
-    dec += decipher.final('utf-8');
+const crypto = require("crypto");
 
-    return dec;
+const cryptoUtil = (function() {
+  const key = Buffer.from("6543211234567890", "utf8");
+  const iv = Buffer.from("1234567890123456", "utf8");
+  const algorithm = "aes-128-cbc";
+
+  function encryption(data) {
+    let secretKey = "";
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    // data 需要加密的数据
+    // utf8 加密数据的字符编码
+    // hex 加密后的字符编码
+    secretKey += cipher.update(data, "utf8", "hex");
+    secretKey += cipher.final("hex");
+
+    return secretKey;
   }
-}
+
+  function decode(secretKey) {
+    let data = "";
+    const cipher = crypto.createDecipheriv(algorithm, key, iv);
+    // secretKey 密匙
+    // hex 加密后的字符编码
+    // utf8 data 解密前的字符编码(还原)
+    data += cipher.update(secretKey, "hex", "utf8");
+    data += cipher.final("utf8");
+
+    return data;
+  }
+
+  return {
+    encryption,
+    decode
+  };
+})();
