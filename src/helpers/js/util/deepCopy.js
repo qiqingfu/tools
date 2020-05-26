cb269dc1047a3f132b6b69cddc3dac26794febe1
qiqf <723469354@qq.com>
2020-05-26 23:23:17 +0800

    feat(util): 更新deepCopy深度克隆对象优化

--- src/helpers/js/util/deepCopy.js
+++ src/helpers/js/util/deepCopy.js
@@ -1,27 +1,52 @@
-import { type } from "/type.js";
+/**
+ * 工具函数
+ * 深度克隆一个对象或数组, 并且使用 cache 缓存
+ */
 
 /**
- * @param {*any} obj
+ * 返回一个数组中符合 f 函数的第一项
+ * @param list
+ * @param f
  */
-export default function deepCopy(data) {
-  const t = type(data);
-  let o;
+const find = function (list, f) {
+  return list.filter(f)[0]
+}
 
-  if (t === "array") {
-    o = [];
-  } else if (t === "object") {
-    o = {};
-  } else {
-    return data;
+/**
+ * 遍历一个对象或数组的 key 和 value
+ * 并且传递给 fn 回调函数
+ * @param obj
+ * @param fn
+ */
+const forEachValue = function (obj, fn) {
+  Object.keys(obj).forEach(key => fn(obj[key], key))
+}
+
+/**
+ * 一个对象的深度克隆
+ * @param obj
+ * @param cache
+ * @returns {[]|{}|ClipboardEvent|*}
+ */
+const deepCopy = function (obj, cache = []) {
+  if (obj === null || typeof obj !== 'object') {
+    return obj
   }
 
-  if (t === "array") {
-    for (let i = 0; i < data.length; i++) o.push(deepCopy(data[i]));
-  } else if (t === "object") {
-    for (let k in data) {
-      o[k] = deepCopy(data[k]);
-    }
+  const hit = find(cache, c => c.original === obj)
+  if (hit) {
+    return hit.copy
   }
 
-  return o;
+  const copy = Array.isArray(obj) ? [] : {}
+  cache.push({
+    original: obj,
+    copy
+  })
+
+  forEachValue(obj, (value, key) => {
+    copy[key] = deepCopy(value, cache)
+  })
+
+  return copy
 }
